176f9694398a577512f762f10257a68f40b548b8
YouCanCode.net <hardpress.dev@proton.me>
2026-05-01 23:27:19 -0400

    Refine title and description in Home component
    Updated the title and description in the Home component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Code fundamentals for all types and ages.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
