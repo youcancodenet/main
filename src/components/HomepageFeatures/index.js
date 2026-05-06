35272bd5642df9ad4252bf195a77710cd073782f
hardpress.dev <hardpress.dev@proton.me>
2026-05-06 14:33:06 -0400

    Updates

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,19 +5,15 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Fun to Learn',
-    // Swapping undraw_docusaurus_mountain.svg for undraw_docusaurus_mountain.png
     Img: require('@site/static/img/undraw_docusaurus_mountain.png').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get a website up and running quickly. We use this open-source tool
-        as a way to teach code fundamentals and web development.  
+        We use Docusaurus as a tool to teach code fundamentals and web development.
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
-    // Swapping undraw_docusaurus_tree.svg for undraw_docusaurus_tree.png
     Img: require('@site/static/img/undraw_docusaurus_tree.png').default,
     description: (
       <>
@@ -28,7 +24,6 @@ const FeatureList = [
   },
   {
     title: 'Powered by React',
-    // Swapping undraw_docusaurus_react.svg for undraw_docusaurus_react.png
     Img: require('@site/static/img/undraw_docusaurus_react.png').default,
     description: (
       <>
@@ -39,13 +34,11 @@ const FeatureList = [
   },
 ];
 
-// Updated component to handle the PNG image source
 function Feature({Img, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        {/* Using <img> tag with the existing styles for consistent sizing */}
-        <img src={Img} className={styles.featureSvg} alt={title} />
+      <div className={styles.featureImgContainer}>
+        <img src={Img} className={styles.featureImg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
