f625a5b0d1acda3df5fdc4d0727d65d48ebee740
YouCanCode.net <hardpress.dev@proton.me>
2026-05-08 16:17:30 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'You Can Code',
-  tagline: 'For all skill types and ages.',
+  tagline: 'Learn fundamentals of coding for all ages.',
   favicon: 'img/favicon.ico',
 
   url: 'https://youcancode.net',
